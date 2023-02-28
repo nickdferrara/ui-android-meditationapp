0e8453ca9b9e68821ac6d67e80a33b6cbca9543d
nickdferrara <nick.d.ferrara@gmail.com>
2023-02-27 21:55:38 -0500

    Added default colors to theme

--- app/src/main/java/com/nickdferrara/meditationapp/ui/theme/Color.kt
+++ app/src/main/java/com/nickdferrara/meditationapp/ui/theme/Color.kt
@@ -2,6 +2,10 @@ package com.nickdferrara.meditationapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
 val TextWhite = Color(0xffeeeeee)
 val DeepBlue = Color(0xff06164c)
 val ButtonBlue = Color(0xff505cf3)
