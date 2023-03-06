66644deec2d16e7a9bbf6d18e7ec26cbf0ef7ada
nickdferrara <nick.d.ferrara@gmail.com>
2023-03-05 21:02:22 -0500

    Make mood chips section

--- app/src/main/java/com/nickdferrara/meditationapp/HomeScreen.kt
+++ app/src/main/java/com/nickdferrara/meditationapp/HomeScreen.kt
@@ -1,17 +1,28 @@
 package com.nickdferrara.meditationapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.nickdferrara.meditationapp.ui.theme.ButtonBlue
+import com.nickdferrara.meditationapp.ui.theme.DarkerButtonBlue
 import com.nickdferrara.meditationapp.ui.theme.DeepBlue
+import com.nickdferrara.meditationapp.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -21,6 +32,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
         }
     }
 }
@@ -58,5 +70,35 @@ fun GreetingSection(
             tint = Color.White,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow() {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                .clickable { selectedChipIndex = it }
+                .clip(RoundedCornerShape(10.dp))
+                .background(
+                    if (selectedChipIndex == it) ButtonBlue
+                    else DarkerButtonBlue
+                )
+                .padding(15.dp)
+            ) {
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+            }
+        }
     }
 }
